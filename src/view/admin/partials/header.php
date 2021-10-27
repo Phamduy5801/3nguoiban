532e569878964c2891cdb4032468d3d35bfcd61a
Phạm Đức Anh <fosster2@gmail.com>
2021-10-27 15:32:48 +0700

    fix duong dan file css

--- src/view/admin/partials/header.php
+++ src/view/admin/partials/header.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="css/admin.css">
     <title> Project cuoi ki </title>
 
 <body>
