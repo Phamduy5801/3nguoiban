89c6f049c441639112f0d6d41af5416948a22929
Phạm Đức Anh <fosster2@gmail.com>
2021-10-27 15:00:02 +0700

    cau truc lai code

--- src/view/student/credit-registration.php
+++ src/view/student/credit-registration.php
@@ -1,4 +1,4 @@
-<?php include('../admin/partials/header.php'); ?>
+<?php include('../student/partials/header.php'); ?>
 <!-- Start main -->
 <div class="main-content">
     <div class="wrapper">
@@ -46,4 +46,4 @@
     </div>
 </div>
 <!-- End main -->
-<?php include('../admin/partials/footer.php'); ?>
+<?php include('../student/partials/footer.php'); ?>
---
+++ src/view/student/partials/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel="stylesheet" href="../css/student.css">
+    <title> Project cuoi ki </title>
+
+<body>
+    <!-- Start header menu -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index-admin.php">Trang chủ</a></li>
+                <li><a href="user.php">Người dùng</a></li>
+                <li><a href="role.php">Chức năng</a></li>
+                <li><a href="#">Sinh viên</a></li>
+                <li><a href="khoa.php">Khoa</a></li>
+                <li><a href="#">Môn học</a></li>
+                <li><a href="#">Giảng viên</a></li>
+                <li><a href="#">Đăng kí học</a></li>
+                <li><a href="#">Đăng xuất</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End header menu -->
