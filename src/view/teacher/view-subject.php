3fc7f41b5d29bf63c8bea91cfaf1204c94ffb3df
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-28 22:26:51 +0700

    hiện thông tin môn học

---
+++ src/view/teacher/view-subject.php
@@ -0,0 +1,79 @@
+
+<?php 
+    // session_start();
+    // if(!isset($_SESSION['done'])){
+    //     header("Location: ../../../login.php");
+    // }
+?>
+<?php include('partials/header.php'); ?>
+<!-- Start main -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1 style="color:#a83232">Danh sách môn học</h1>
+        <br>
+        
+       
+        <br><br>
+        <table class="table table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Khoa</th>
+                    <th>Tên</th>
+                    <th>Ngày bắt đầu</th>
+                    <th>Ngày kết thúc</th>
+                    <th>Thời gian học</th>
+                    <th>Giảng viên</th>
+                    <th>Số tín chỉ</th>
+                    
+                   
+                </tr>
+            </thead>
+                <?php
+                //gọi file config để kết nô
+                include_once ("../../config/config.php");
+                //câu lệNh sql
+                $query = "Select sb_id,ten_khoa, sb_ten,ngay_batdau, ngay_ketthuc, thoigian_hoc, tea_ten, sb_tinchi   from db_subject , db_khoa , db_teacher
+                where db_khoa.ma_khoa = db_subject.ma_khoa and db_teacher.tea_id = db_subject.tea_id;";
+                // thực thi câu lệnh sql
+                $result = $conn->query($query);
+                //kiểm tra sự tồn tại của $result
+                if (!$result) {
+                    //nến không có thì die tại đây là hiển thị lỗi
+                    die($conn->error);
+                }
+                //khởi tạo $role = 1 mảng
+                $subject = array();
+                //chạy vòng lặp để lấy dữ liệu theo từng hàng 
+                while ($r = $result->fetch_array(MYSQLI_BOTH)) {
+                    $subject[] = $r;
+                }
+                //duyệt dữ liệu từ mảng gán vào biến $st
+                for ($i = 0; $i < count($subject); $i++) {
+                    $sb = $subject[$i];
+                ?>
+                    <tr>
+                        <!-- lấy dữ liệu từ mảng hiển thị lên bảng -->
+                        
+                        <td><?php echo $sb['sb_id'] ?></td>
+                        <td><?php echo $sb['ten_khoa'] ?></td>
+                        <td><?php echo $sb['sb_ten'] ?></td>
+                        <td><?php echo $sb['ngay_batdau'] ?></td>
+                        <td><?php echo $sb['ngay_ketthuc'] ?></td>
+                        <td><?php echo $sb['thoigian_hoc'] ?></td>
+                        <td><?php echo $sb['tea_ten'] ?></td>
+                        <td><?php echo $sb['sb_tinchi'] ?></td>                      
+                    </tr>
+                <?php
+                }
+                ?>
+
+            </table>
+        
+        <div class="clearfix"></div>
+
+    </div>
+</div>
+<!-- End main -->
+
+<?php include('partials/footer.php'); ?>
