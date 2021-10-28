dea6210f2c0e50a5d78e86652607a932d57f97a8
Phạm Đức Anh <fosster2@gmail.com>
2021-10-28 13:27:25 +0700

    new config

--- src/config/config.php
+++ src/config/config.php
@@ -5,10 +5,8 @@ const Host = 'localhost';
 const User = 'root';
 const Pass = '';
 const DB = 'projectcuoiki';
-$conn = mysqli_connect(Host, User, Pass, DB);
+$conn = new mysqli(Host, User, Pass, DB);
 if (!$conn) {
-    die('Không thể kết nối');
-} else {
-    // echo "Kết nối thành công";
+    die('Không thể kết nối'.$conn->connectio_error);
 }
 ?>
