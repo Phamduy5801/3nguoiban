9de92a92ea628027998189f4300c5958fd5fd7e0
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-28 14:08:28 +0700

    Create index.php
    index teacher

---
+++ src/view/teacher/index.php
@@ -0,0 +1,41 @@
+
+<?php
+include("partials/header.php")
+?>
+<div class="main-content">
+    <div class="wrapper" style="width: 100%;">
+        <div class="container">
+            <!-- Lựa chọn của admin -->
+            <div class="col-6 text-center box">
+            <a href="#">
+                <div class="icon-group">
+                    <i class="fas fa-university"></i>
+                </div>
+                Thông tin cá nhân
+                </a>
+            </div>
+             <!-- Lựa chọn của admin -->
+            <div class="col-6 text-center box">
+                <a href="#">
+                <div class="icon-group">
+                    <i class="fas fa-university"></i>
+                </div>
+                Khoa
+                </a>
+            </div>
+            <div class="col-6 text-center box">
+                <a href="#">
+                <div class="icon-group">
+                    <i class="fas fa-university"></i>
+                </div>
+                Giảng viên
+                </a>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+
+    </div>
+</div>
+<?php
+include("partials/footer.php")
+?>
