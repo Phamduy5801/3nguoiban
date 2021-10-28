42f5ef1ea2436fe152ef33df7085ddca8f79f01a
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-28 14:10:30 +0700

    phần partials

---
+++ src/view/teacher/partials/footer.php
@@ -0,0 +1,19 @@
+
+ <!-- Start footer -->
+ <div class="footer">
+        <div class="wrapper">
+            
+             <p class="text-center">2021 Bảo lưu mọi quyền.</p> 
+        </div>
+    </div>
+    <!-- End footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+     <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+ <!-- JavaScript Bundle with Popper -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    
+</body>
+
+</html>
---
+++ src/view/teacher/partials/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+<link rel="stylesheet" href="css/teacher.css">
+<body>
+<div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Thông tin cá nhân</a></li>
+                <li><a href="list-teacher.php">Giảng viên</a></li>
+                <li><a href="list-student.php">Danh sách sinh viên</a></li>
+                <li><a href="update-subject.php">Môn học</a></li>
+                <li><a href="../../../logout.php">Đăng xuất</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End header menu -->
