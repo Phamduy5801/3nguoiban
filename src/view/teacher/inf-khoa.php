fa0aa54478dc4584e17892608c84949ee69e0b59
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-29 10:08:31 +0700

    thông báo khoa

--- src/view/teacher/inf-khoa.php
+++ src/view/teacher/inf-khoa.php
@@ -1,56 +1,55 @@
-<?php
-session_start();
-if (!isset($_SESSION['done'])) {
-    header("Location: ../../../login.php");
-}
-?> 
+<?php 
+    session_start();
+    if(!isset($_SESSION['done'])){
+        header("Location: ../../../login.php");
+    }
+?>
 <?php include('partials/header.php'); ?>
+
 <!-- Start main -->
 <div class="main-content">
     <div class="wrapper">
-        <h1 style="color:#a83232">Thông báo khoa</h1>
+        <h1 style="color:#a83232">Quản lý khoa</h1>
         <br>
         <div class="container">
-            <br><br>
             <table class="table table-striped">
-                <thead class="table-dark">
-                    <tr>
-                        <th class="d-flex justify-content-center">ID</th>
-                        <th>Tên</th>
-                        <th>Thông báo</th>
-                          
-                                         
-                    </tr>
-                </thead>
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Tên</th>
+                    <th>Thông tin</th>
+                    <th>Sửa</th>
+                </tr>
+            </thead>
                 <?php
                 //gọi file config để kết nô
-                include_once("../../config/config.php");
+                include_once ("config/config.php");
                 //câu lệNh sql
-                $query = "Select ma_khoa, ten_khoa, tb_khoa from db_khoa ";
-                // thực thi câu lệnh sql
+                $query = "Select  ma_khoa, ten_khoa, tb_khoa  from  db_khoa ";
+                // thực thi câu lệnh sql 
                 $result = $conn->query($query);
                 //kiểm tra sự tồn tại của $result
                 if (!$result) {
-                    //nến không có thì die tại đây là hiển thị lỗi
                     die($conn->error);
                 }
-                //khởi tạo $role = 1 mảng
-                $student = array();
-                //chạy vòng lặp để lấy dữ liệu theo từng hàng 
+                //khởi tạo $khoa = 1 mảng
+                $khoa = array();
+                //chạy vòng lặP while và lấy dữ liệu theo từng hàng
                 while ($r = $result->fetch_array(MYSQLI_BOTH)) {
-                    $student[] = $r;
+                    $khoa[] = $r;
                 }
-                //duyệt dữ liệu từ mảng gán vào biến $st
-                for ($i = 0; $i < count($student); $i++) {
-                    $st = $student[$i];
+                //duyệt dữ liệu từ mảng gán vào biến $k
+                for ($i = 0; $i < count($khoa); $i++) {
+                    $k = $khoa[$i];
                 ?>
                     <tr>
                         <!-- lấy dữ liệu từ mảng hiển thị lên bảng -->
-                        <td colspan="1"><?php echo $st['ma_khoa'] ?></td>
-                        <td colspan="1"><?php echo $st['ten_khoa'] ?></td>
-                        <td colspan="1"><?php echo $st['tb_khoa'] ?></td>
-                        
-                        
+                        <td><?php echo $k['ma_khoa'] ?></td>
+                        <td><?php echo $k['ten_khoa'] ?></td>
+                        <td><?php echo $k['tb_khoa'] ?></td>
+                        <td>
+                            <a class="btn btn-success bt-add" href="update-inf-khoa.php?ma_khoa=<?php echo $k['ma_khoa'] ?>" value=""><i class="fa-solid fa-wrench"></i></a>
+                        </td>
                     </tr>
                 <?php
                 }
@@ -64,4 +63,4 @@ if (!isset($_SESSION['done'])) {
 </div>
 <!-- End main -->
 
-<?php include('partials/footer.php'); ?>
+<?php include('partials/footer.php'); ?> 
