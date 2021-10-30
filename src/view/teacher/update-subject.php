c28db38026682f3ca206273adc42adb7ae5ea07f
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-30 19:40:02 +0700

    thay đổi tbao của môn học

--- src/view/teacher/update-subject.php
+++ src/view/teacher/update-subject.php
@@ -25,6 +25,7 @@
                     <th>Thời gian học</th>
                     <th>Giảng viên</th>
                     <th>Số tín chỉ</th>
+                    <th>Thông báo</th>
                     <th>Cập nhập</th>
                    
                 </tr>
@@ -33,7 +34,7 @@
                 //gọi file config để kết nô
                 include_once ("config/config.php");
                 //câu lệNh sql
-                $query = "Select sb_id,ten_khoa, sb_ten,ngay_batdau, ngay_ketthuc, thoigian_hoc, tea_ten, sb_tinchi   from db_subject , db_khoa , db_teacher
+                $query = "Select sb_id,ten_khoa, sb_ten,ngay_batdau, ngay_ketthuc, thoigian_hoc, tea_ten, sb_tinchi, sb_tb  from db_subject , db_khoa , db_teacher
                 where db_khoa.ma_khoa = db_subject.ma_khoa and db_teacher.tea_id = db_subject.tea_id;";
                 // thực thi câu lệnh sql
                 $result = $conn->query($query);
@@ -63,6 +64,7 @@
                         <td><?php echo $sb['thoigian_hoc'] ?></td>
                         <td><?php echo $sb['tea_ten'] ?></td>
                         <td><?php echo $sb['sb_tinchi'] ?></td>
+                        <td>Tài liệu: <?php echo $sb['sb_tb'] ?></td>
                         <td>
                             <a class="btn btn-success bt-add" href=" add-subject.php?sb_id=<?php echo $sb['sb_id']?>"><i class="fas fa-edit"></i></a>
                             
