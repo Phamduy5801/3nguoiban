21048b56553ee1e1b33e9c0ef45120264aadc8cb
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-30 19:37:50 +0700

    sửa session và design lại list-student

--- src/view/teacher/list-student.php
+++ src/view/teacher/list-student.php
@@ -1,19 +1,22 @@
-@@ -0,0 +1,81 @@
-<?php
-session_start();
-if (!isset($_SESSION['done'])) {
-    header("Location: ../../../login.php");
-}
+
+<?php 
+    session_start();
+    if(!isset($_SESSION['teacher'])){
+        header("Location: ../../../login.php");
+    }
 ?>
+
 <?php include('partials/header.php'); ?>
 <!-- Start main -->
 <div class="main-content">
     <div class="wrapper">
         <h1 style="color:#a83232">Danh sách sinh viên</h1>
         <br>
+        
         <div class="container">
-            <a class="btn btn-primary bt-add" href="add-student.php" value="">Thêm mới sinh viên</a>
             <br><br>
+            
+            <br>
             <table class="table table-striped">
                 <thead class="table-dark">
                     <tr>
@@ -23,14 +26,13 @@ if (!isset($_SESSION['done'])) {
                         <th>Số điện thoại</th>
                         <th>Email</th>
                         <th>Địa chỉ</th>
-                        <th>Khoa</th>
-                        <th>Sửa</th>
-                        <th>Xóa</th>
+                        <th>Khoa</th>  
+                                         
                     </tr>
                 </thead>
                 <?php
                 //gọi file config để kết nô
-                include_once("../../config/config.php");
+                include_once("config/config.php");
                 //câu lệNh sql
                 $query = "Select st_id, st_ten, st_lop, st_sdt, st_email, st_diachi, ten_khoa  from db_student , db_khoa 
                 where db_khoa.ma_khoa = db_student.ma_khoa  ";
@@ -60,12 +62,7 @@ if (!isset($_SESSION['done'])) {
                         <td colspan="1"><?php echo $st['st_email'] ?></td>
                         <td colspan="1"><?php echo $st['st_diachi'] ?></td>
                         <td colspan="1"><?php echo $st['ten_khoa'] ?></td>
-                        <td colspan="1">
-                            <a class="btn btn-success bt-add" href="update-student.php?st_id=<?php echo $st['st_id']  ?>"><i class="fa-solid fa-wrench"></i></a>
-                        </td>
-                        <td colspan="1">
-                            <a onclick="return confirm('Bạn có chắc xoá sinh viên <?php echo $st['st_ten'] ?> ?')" class="btn btn-danger" href="delete-student.php?st_id=<?php echo $st['st_id'] ?>"><i class="fa-solid fa-user-slash"></i></a>
-                        </td>
+                        
                     </tr>
                 <?php
                 }
@@ -77,6 +74,7 @@ if (!isset($_SESSION['done'])) {
 
     </div>
 </div>
+
 <!-- End main -->
 
 <?php include('partials/footer.php'); ?>
