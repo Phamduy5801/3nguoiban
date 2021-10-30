a217e6d4dd36e61b814bbd19c069b94e1a5acd24
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-30 09:58:01 +0700

    đề xuất của gvien

--- index1.php
+++ index1.php
@@ -12,7 +12,7 @@
     <title>Hello, world!</title>
 </head>
 <?php
-include("../3nguoiban/src/config/config.php");//abc 
+include("../3nguoiban/src/config/config.php");//abcdèf
 ?>
 
 <body>
--- src/view/teacher/de-xuat.php
+++ src/view/teacher/de-xuat.php
@@ -3,7 +3,7 @@
     if(!isset($_SESSION['teacher'])){
         header("Location: ../../../login.php");
     }
- ?>
+?>
 <?php
 include("partials/header.php")
 ?>
@@ -11,25 +11,25 @@ include("partials/header.php")
 <div class="main-content">
     <div class="wrapper" style="width: 100%;">
         <div class="container">
-        <h1 style="color:#a83232">Trang chủ giảng viên</h1>
+        <h1 style="color:#a83232">Chỉnh sửa thông tin</h1>
         <br><br>
             <!-- Lựa chọn của admin -->
             <div class="col-6 text-center box">
-            <a href="list-teacher.php">
+            <a href="dexuat-list-teacher.php">
                 <div class="icon-group">
-                    <i class="fas fa-university"></i>
+                    <i class="fas fa-user-edit"></i>
                 </div>
-                Thông tin Giảng viên
+                 Giảng viên
                 </a>
             </div>
              <!-- Lựa chọn của admin -->
 
             <div class="col-6 text-center box">
-                <a href="view-subject.php">
+                <a href="dexuat-list-student.php">
                 <div class="icon-group">
-                    <i class="fas fa-university"></i>
+                    <i class="fas fa-user-edit"></i>
                 </div>
-                Danh sách môn học
+                Sinh viên
                 </a>
             </div>
         </div>
