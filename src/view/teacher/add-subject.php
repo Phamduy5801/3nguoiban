de1210d59846f74154546a9df8b2162457dee0c7
nguyenminhvu4901 <90379036+nguyenminhvu4901@users.noreply.github.com>
2021-10-30 20:14:48 +0700

    thay đổi thông tin thêm môn học

--- src/view/teacher/add-subject.php
+++ src/view/teacher/add-subject.php
@@ -1,50 +1,55 @@
+<?php 
+    session_start();
+    if(!isset($_SESSION['teacher'])){
+        header("Location: ../../../login.php");
+    }
+?>
 <?php include('partials/header.php'); ?>
+<!-- Start main -->
 <div class="main-content">
     <div class="wrapper">
-        <h1 style="color:#a83232">Thêm thông tin môn học</h1>
-        <br><br>
+        <h1 style="color:#a83232">Cập nhật thông tin môn học</h1>
+        <br>
+        <?php
+        //kiểm tra khởi tạo của biến sb_id
+        if (isset($_GET['sb_id'])) {
+            //gán $id = dữ liệu của sb_id
+            $id = $_GET['sb_id'];
+        } else {
+            //nếu khgông đưuọc thì quay về trang subject.php
+            header("Location: update-subject.php");
+        }
+        //kết nối csdl 
+        include_once ("config/config.php");
+        //câu lệnh sql
+        $caulenh = "Select * from db_subject where sb_id=?";
+        //tạo đỐi tượng prepare
+        $stmt = $conn->prepare($caulenh);
+        // Gán giá trị vào các tham số ẩn ('?')
+        $stmt->bind_param("s", $id);
+        //thực thi câu lệnh sql
+        $stmt->execute();
+        //tạo biến $result lấy dữ liệU của $stmt
+        $result = $stmt->get_result();
+        //kiểm tra $result
+        if (!$result) {
+            //nếU sai thì die tại đây và hiển thị lỗi
+            die($conn->error);
+        }
+        //tạo biến $sb và lấy dữ liệu theo từng hàng 
+        $sb = $result->fetch_array(MYSQLI_BOTH);
+        
+        
+        ?>
         <div class="container">
-            <form method="POST" action="add-subject.php">
-                ID: <input class="form-control" type="text"readonly name="id"/><br>
-                Khoa: 
-                <!-- tạo ra 1 seclect box cho người dùng lựa chọn danh sách tên khoa -->
-                <select class="form-select form-control" name="khoa" aria-label="Default select example">
-                    <?php
-                    //kết nối csdl
-                    include_once ("../../config/config.php");
-
-                    //câu lệnh sql
-                    $query = "Select ma_khoa, ten_khoa from db_khoa";
-                    //thực thi câu lệnh sql
-                    $result = $conn->query($query);
-                    //nếU không thực thi đc thì hiển thị lỗi
-                    if (!$result) {
-                        die($conn->error);
-                    }
-                    //gán biến $khoa vào 1 mảng
-                    $khoa = array();
-                    //chạy vòng lặp để lấy dữ liệu theo từng hàng 
-                    while ($r = $result->fetch_array(MYSQLI_BOTH)) {
-                        $khoa[] = $r;
-                    }
-
-                    // sử dụng vòng lặp foreach để duyệt dữ liệu trong mảng và gán vào biến $kh
-                    foreach ($khoa as $kh) {
-                    ?>
-                        <!-- cho admin lựa chọn option với value là role_id và hiển thị trên web là tên của cái role tương ứng với role_id đÓ -->
-                        <option value="<?php echo $kh['ma_khoa'] ?>"><?php echo $kh['ten_khoa'] ?></option>
-                    <?php
-                    }
-                    ?>
-                </select>
+            <form method="POST" action="add-subject.php?sb_id=<?php echo $id ?>">
+                Tên: <input class="form-control" type="text" readonly name="ten" value="<?php echo $sb['sb_ten'] ?>" /><br>
+                Ngày bắt đầu: <input class="form-control" readonly type="date" name="ngay_bd" value="<?php echo $sb['ngay_batdau'] ?>" /><br>
+                Ngày kết thúc: <input class="form-control" readonly type="date" name="ngay_kt" value="<?php echo $sb['ngay_ketthuc'] ?>" /><br>
+                Thời gian học: <input class="form-control" type="text" name="thoigian_hoc" value="<?php echo $sb['thoigian_hoc'] ?>" /><br>
+                Thông báo: <input class="form-control" type="text" name="sb_tb" value="<?php echo $sb['sb_tb'] ?>" /><br>
                 <br>
-                Tên: <input class="form-control" type="text" name="ten"/><br>
-                Ngày bắt đầu: <input class="form-control" type="date" name="ngay_batdau"/><br>
-                Ngày kết thúc: <input class="form-control" type="date" name="ngay_ketthuc"><br>
-                Thời gian học: <input class="form-control" type="text" name="thoigian_hoc"/><br>
-                Giảng viên: <input class="form-control" type="text" name="giang_vien"/><br>
-                Số tín chỉ: <input class="form-control" type="text" name="so_tc"/><br>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
         <div class="clearfix"></div>
@@ -52,36 +57,32 @@
 </div>
 <!-- End main -->
 
-
 <?php
-//kiểm tra dữ liệu người dùng nhậP
-if (isset($_REQUEST['id']) && isset($_REQUEST['khoa']) && isset($_REQUEST['ten']) && isset($_REQUEST['ngay_batdau']) && isset($_REQUEST['ngay_ketthuc']) && isset($_REQUEST['thoigian_hoc'])  && isset($_REQUEST['giang_vien']) && isset($_REQUEST['so_tc'])) {
-
-    //kết nối sql
-    include_once ("../../config/config.php");
+//kiểm tra ten đã tồn tại hay chưa
+if (isset($_REQUEST['ten']) && isset($_REQUEST['ngay_bd']) && isset($_REQUEST['ngay_kt'])  && isset($_REQUEST['thoigian_hoc']) && isset($_REQUEST['sb_tb']) ) {
+    //gán $id = dữ liệu của sb_id
+    $id = $_GET['sb_id'];
+    //kết nối cơ sở dữ liệu
+    include_once ("config/config.php");
     //câu lệnh sql
-    $query = "INSERT INTO `db_subject` (`sb_id`,`ma_khoa`,`sb_ten`,`ngay_batdau`,`ngay_ketthuc`,`thoigian_hoc`,`giang_vien`,`sb_tinchi`) VALUES (?,?,?,?,?,?,?,?);";
-    // Tạo đối tượng repare
+    $query = "UPDATE `db_subject` SET `sb_ten` = ?, `ngay_batdau` = ?, `ngay_ketthuc` = ? ,`thoigian_hoc` = ?,`sb_tb` = ?   WHERE `db_subject`.`sb_id` = ?";
+    
+    //khởI tạo đố itượng prepare
     $stmt = $conn->prepare($query);
-    //gán dữ liệu vào biến và lấy dữ liệu từ form
-
-    $id = $_POST['id'];
-    $khoa = $_POST['khoa'];
+    //gán biến = dữ liệu lấy từ trong form
     $ten = $_POST['ten'];
-    $ngay_bd = $_POST['ngay_batdau'];
-    $ngay_kt = $_POST['ngay_ketthuc'];
-    $thoigian_hoc = $_POST['thoigian_hoc'];
-    $giangvien = $_POST['giang_vien'];
-    $so_tc = $_POST['so_tc'];
-
+    $ngay_bd = $_POST['ngay_bd'];
+    $ngay_kt = $_POST['ngay_kt'];
+    $thoigian = $_POST['thoigian_hoc'];
+    $sb_tb = $_POST['sb_tb'];
     // Gán giá trị vào các tham số ẩn ('?')
-    $stmt->bind_param('sssssssd', $id, $khoa, $ten, $ngay_bd, $ngay_kt, $thoigian_hoc, $giangvien, $so_tc);
-    //thực thi câu truy vấn
+    $stmt->bind_param("ssssss", $ten, $ngay_bd, $ngay_kt, $thoigian, $sb_tb, $id);
+    //thực thi câu lệnh sql
     $stmt->execute();
-    //đóng kết nối cơ sở dữ liệu
+    //đóng kếT nối
     $conn->close();
-    //chuyển về trang student
-    header("Location: subject.php");
+    //trở lại trang subject.php
+    header("Location: update-subject.php");
 }
 ?>
 <?php include('partials/footer.php'); ?>
