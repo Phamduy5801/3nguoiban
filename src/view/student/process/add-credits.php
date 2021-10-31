0eeca674e61e75759b308c9274f4c27a15940b85
Phạm Đức Anh <fosster2@gmail.com>
2021-10-31 17:43:34 +0700

    sua lai add credits

--- src/view/student/process/add-credits.php
+++ src/view/student/process/add-credits.php
@@ -4,6 +4,7 @@ include('../../../config/config.php');
 $username = $_SESSION['student'];
 $mamon = $_POST['mamon'];
 if ($mamon != null) {
+    // check xem mon day co ton tai hay ko nếu có lấy ra mã môn học
     $sql = "select sb_id from db_subject where sb_id = '$mamon'";
     $result = mysqli_query($conn, $sql);
     if ($result == true) {
@@ -14,6 +15,7 @@ if ($mamon != null) {
         } else {
             header('Location:' . SITESDURL . 'credits-registration.php');
         }
+        // lay ma sinh vien co username da dc dang nhap trong phien lam viec
         $sql1 = "SELECT `st_id` FROM `db_student`, `db_user` WHERE db_user.user_id = db_user.user_id and db_user.username = '$username'";
         $result1 = mysqli_query($conn, $sql1);
         if ($result1 == true) {
@@ -21,20 +23,25 @@ if ($mamon != null) {
                 while ($row = mysqli_fetch_assoc($result1)) {
                     $st_id = $row['st_id'];
                 }
-            $sql2 = "SELECT COUNT(*) FROM `db_dkihoc` WHERE sb_id LIKE '$sb_id'";
+                // check xem mon day da dc dang ki chua
+            $sql2 = "SELECT db_dkihoc.sb_id, db_student.st_id
+            FROM `db_dkihoc`, `db_user`,`db_student` 
+            WHERE db_dkihoc.st_id = db_student.st_id and db_student.user_id = db_user.user_id and  and db_user.username = '$username' and db_dkihoc.sb_id ='$mamon'";
             $result2 = mysqli_query($conn, $sql2);
-            $row = mysqli_fetch_assoc($result2);
-            $count = $row['COUNT(*)'];
-            if ($count < 1) {
-                $today = date("Y/m/d");
-                $sql3 = "INSERT INTO `db_dkihoc`(`sb_id`, `st_id`, `ngay_dki`)
+            if ($result2 == true) {
+                $rowcount = mysqli_num_rows($result2);
+                // neu ma chua dc dang ki thi ko co ban ghi nao thi se dc dang ki
+                if ($rowcount < 1) { 
+                    $today = date("Y/m/d");
+                    $sql3 = "INSERT INTO `db_dkihoc`(`sb_id`, `st_id`, `ngay_dki`)
         VALUES ('$sb_id','$st_id','$today')";
-                $result3 = mysqli_query($conn, $sql3);
-                if ($result3 > 0) {
+                    $result3 = mysqli_query($conn, $sql3);
+                    if ($result3 > 0) {
+                        header('Location:' . SITESDURL . 'credits-registration.php');
+                    }
+                } else {
                     header('Location:' . SITESDURL . 'credits-registration.php');
                 }
-            } else {
-                header('Location:' . SITESDURL . 'credits-registration.php');
             }
         }
     }
